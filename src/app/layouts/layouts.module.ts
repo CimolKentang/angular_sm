0d3dd4411b095fe2cbbf1f7c7a7506be76289a20
CimolKentang <cimolkentang245@gmail.com>
2024-07-08 20:03:04 +0700

    generate beberapa module

---
+++ src/app/layouts/layouts.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { PostLayoutComponent } from './post-layout/post-layout.component';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule
+  ]
+})
+export class LayoutsModule { }
---
+++ src/app/layouts/post-layout/post-layout.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-post-layout',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './post-layout.component.html',
+  styleUrl: './post-layout.component.css'
+})
+export class PostLayoutComponent {
+
+}
